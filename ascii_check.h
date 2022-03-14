fb29a50c0d0cb6c176f5c1d0ab6f49e5f5fff834
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-03-14 11:33:49 +0100

    Add ASCII check from dnaio and start on test program

---
+++ ascii_check.h
@@ -0,0 +1,59 @@
+// Copyright (c) 2022 Ruben Vorderman
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#define ASCII_MASK_8BYTE 0x8080808080808080ULL
+#define ASCII_MASK_1BYTE 0x80
+
+#include <stddef.h>
+#include <stdint.h>
+
+static int
+string_is_ascii(char * string, size_t length) {
+    size_t n = length;
+    char * char_ptr = string;
+    // The first loop aligns the memory address. Char_ptr is cast to a size_t
+    // to return the memory address. Uint64_t is 8 bytes long, and the processor
+    // handles this better when its address is a multiplier of 8. This loops
+    // handles the first few bytes that are not on such a multiplier boundary.
+    while ((size_t)char_ptr % sizeof(uint64_t) && n != 0) {
+        if (*char_ptr & ASCII_MASK_1BYTE) {
+            return 0;
+        }
+        char_ptr += 1;
+        n -= 1;
+    }
+    uint64_t *longword_ptr = (uint64_t *)char_ptr;
+    while (n >= sizeof(uint64_t)) {
+        if (*longword_ptr & ASCII_MASK_8BYTE){
+            return 0;
+        }
+        longword_ptr += 1;
+        n -= sizeof(uint64_t);
+    }
+    char_ptr = (char *)longword_ptr;
+    while (n != 0) {
+        if (*char_ptr & ASCII_MASK_1BYTE) {
+            return 0;
+        }
+        char_ptr += 1;
+        n -= 1;
+    }
+    return 1;
+}

f15cfdb3cdb9bda3720e760b96cda9fb17c0e657
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-03-14 16:19:59 +0100

    Developed in work time

--- ascii_check_sse41.h
+++ ascii_check_sse41.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Ruben Vorderman
+// Copyright (c) 2022 Leiden University Medical Center
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to 
